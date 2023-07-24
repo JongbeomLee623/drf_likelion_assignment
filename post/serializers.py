16cc1d3e6793743d9ecde5fa997f3ae3b3b3b124
JongbeomLee623 <jambon23@dgu.ac.kr>
2023-07-25 01:41:20 +0900

    drf 여름 4주차 과제

--- post/serializers.py
+++ post/serializers.py
@@ -28,8 +28,28 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
+        # exclude = ('like_cnt','like')
         fields = '__all__'
-        read_only_field = ['id', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at','comments','like','like_cnt']
+        
+
+class PostListSerializer(serializers.ModelSerializer):
+    comments_cnt =  serializers.SerializerMethodField()
+    
+    def get_comments_cnt(self, instance):
+        return instance.comments.count()
+    
+    tag = serializers.SerializerMethodField()
+    def get_tag(self, instance):
+        tags = instance.tag.all()
+        return [tag.name for tag in tags]
+    
+    image =  serializers.ImageField(use_url = True, required = False)
+
+    class Meta:
+        model = Post
+        fields = ['id','writer','created_at','updated_at','comments_cnt','tag','image','like_cnt']
+        read_only_field = ['id', 'created_at', 'updated_at','comments_cnt']
 
 class TagSerializer(serializers.ModelSerializer):
 
--- post/views.py
+++ post/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets, mixins
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
 
 from .models import *
 from .serializers import *
+from .permissions import IsOwnerOrReadOnly
 
 from django.conf import settings
 import os
@@ -12,7 +15,17 @@ import os
 class PostViewSet(viewsets.GenericViewSet,
     mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin, mixins.UpdateModelMixin):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    # serializer_class = PostSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+        return PostSerializer
+    
+    def get_permissions(self):
+        if self.action in ["create","update","destroy","partial_update"]:
+            return [IsAdminUser()]
+        return []
 
     def create(self, request):
         serializer = self.get_serializer(data =  request.data)
@@ -47,23 +60,59 @@ class PostViewSet(viewsets.GenericViewSet,
 
         post.save()
 
+    @action(methods=["GET"],detail=False)
+    def recommend(self, request):
+        recommended_post = self.get_queryset().order_by("-like_cnt")[:3]
+        recommended_post_serializer = PostListSerializer(recommended_post, many = True)
+        return Response(recommended_post_serializer.data)
+    
+    @action(methods=['GET'],detail=True)
+    def like(self, request, pk = None):
+        liked_post = self.get_object()
+        user = request.user
+        if user in liked_post.like.all():
+            liked_post.like.remove(user)
+            liked_post.like_cnt -= 1
+        else:
+            liked_post.like.add(user)
+            liked_post.like_cnt += 1
+
+        liked_post.save(update_fields=["like_cnt"])
+        return Response()
+
 class CommentViewSet(viewsets.GenericViewSet,
     mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
     queryset = Comment.objects.all()
     serializer_class =  CommentSerializer
 
+    def get_permissions(self):
+        if self.action in ["create","update","destroy","partial_update"]:
+            return [IsOwnerOrReadOnly()]
+        return []
+    
+    def get_object(self):
+        obj = super().get_object()
+        return obj
+
 
 class PostCommentViewSet(viewsets.GenericViewSet,
     mixins.ListModelMixin, mixins.CreateModelMixin):
-    queryset = Comment.objects.all()
+    # queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
 
-    def list(self, request, post_id = None):
-        post = get_object_or_404(Post, id = post_id)
-        queryset = self.filter_queryset(self.get_queryset().filter(post=post))
-        serializer = self.get_serializer(queryset, many = True)
-        return Response(serializer.data)
-    
+    # def list(self, request, post_id = None):
+    #     post = get_object_or_404(Post, id = post_id)
+    #     queryset = self.filter_queryset(self.get_queryset().filter(post=post))
+    #     serializer = self.get_serializer(queryset, many = True)
+    #     return Response(serializer.data)
+
+    def get_queryset(self):
+        post = self.kwargs.get("post_id")
+        queryset = Comment.objects.filter(post_id = post)
+        return queryset
+
+
     def create(self, request, post_id = None):
         post = get_object_or_404(Post, id = post_id)
         serializer = self.get_serializer(data = request.data)
